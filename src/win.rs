DanielKeep/directories-rs

commit cb6a274d1b07d91e739e2507441b389054597c69

Add support for `XDG_STATE_HOME`

src/win.rs
@@ -80,7 +80,8 @@ pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> {
             data_dir:       data_dir,
             data_local_dir: data_local_dir,
             preference_dir: preference_dir,
-            runtime_dir:    None
+            runtime_dir:    None,
+            state_dir:      None
         };
         Some(project_dirs)
     } else {

src/lib.rs
@@ -72,6 +72,7 @@ pub struct BaseDirs {
     executable_dir: Option<PathBuf>,
     preference_dir: PathBuf,
     runtime_dir:    Option<PathBuf>,
+    state_dir:      Option<PathBuf>
 }
 
 /// `UserDirs` provides paths of user-facing standard directories, following the conventions of the operating system the library is running on.
@@ -134,7 +135,8 @@ pub struct ProjectDirs {
     data_dir:       PathBuf,
     data_local_dir: PathBuf,
     preference_dir: PathBuf,
-    runtime_dir:    Option<PathBuf>
+    runtime_dir:    Option<PathBuf>,
+    state_dir:      Option<PathBuf>
 }
 
 impl BaseDirs {
@@ -245,6 +247,22 @@ impl BaseDirs {
     /// | Windows | –                  | –               |
     pub fn runtime_dir(&self) -> Option<&Path> {
         self.runtime_dir.as_ref().map(|p| p.as_path())
+    }
+    /// Returns the path to the user's state directory.
+    ///
+    /// The state directory contains data that should be retained between sessions (unlike the runtime
+    /// directory), but may not be important/portable enough to be synchronized across machines (unlike
+    /// the config/preferences/data directories).
+    ///
+    /// The returned value depends on the operating system and is either a `Some`, containing a value from the following table, or a `None`.
+    ///
+    /// |Platform | Value                                     | Example                  |
+    /// | ------- | ----------------------------------------- | ------------------------ |
+    /// | Linux   | `$XDG_STATE_HOME` or `$HOME`/.local/state | /home/alice/.local/state |
+    /// | macOS   | –                                         | –                        |
+    /// | Windows | –                                         | –                        |
+    pub fn state_dir(&self) -> Option<&Path> {
+        self.state_dir.as_ref().map(|p| p.as_path())
     }
 }
 
@@ -462,6 +480,22 @@ impl ProjectDirs {
     /// | Windows | –                                   | –                     |
     pub fn runtime_dir(&self) -> Option<&Path> {
         self.runtime_dir.as_ref().map(|p| p.as_path())
+    }
+    /// Returns the path to the project's state directory.
+    ///
+    /// The state directory contains data that should be retained between sessions (unlike the runtime
+    /// directory), but may not be important/portable enough to be synchronized across machines (unlike
+    /// the config/preferences/data directories).
+    ///
+    /// The returned value depends on the operating system and is either a `Some`, containing a value from the following table, or a `None`.
+    ///
+    /// |Platform | Value                                                                       | Example                         |
+    /// | ------- | --------------------------------------------------------------------------- | ------------------------------- |
+    /// | Linux   | `$XDG_STATE_HOME`/`_project_path_` or `$HOME`/.local/state/`_project_path_` | /home/alice/.local/state/barapp |
+    /// | macOS   | –                                                                           | –                               |
+    /// | Windows | –                                                                           | –                               |
+    pub fn state_dir(&self) -> Option<&Path> {
+        self.state_dir.as_ref().map(|p| p.as_path())
     }
 }
 

src/lin.rs
@@ -15,6 +15,7 @@ pub fn base_dirs() -> Option<BaseDirs> {
         let data_local_dir = data_dir.clone();
         let preference_dir = config_dir.clone();
         let runtime_dir    = env::var_os("XDG_RUNTIME_DIR").and_then(dirs_sys::is_absolute_path);
+        let state_dir      = env::var_os("XDG_STATE_HOME") .and_then(dirs_sys::is_absolute_path).unwrap_or_else(|| home_dir.join(".local/state"));
         let executable_dir =
             env::var_os("XDG_BIN_HOME").and_then(dirs_sys::is_absolute_path).unwrap_or_else(|| {
                 let mut new_dir = data_dir.clone(); new_dir.pop(); new_dir.push("bin"); new_dir });
@@ -27,7 +28,8 @@ pub fn base_dirs() -> Option<BaseDirs> {
             data_local_dir: data_local_dir,
             executable_dir: Some(executable_dir),
             preference_dir: preference_dir,
-            runtime_dir:    runtime_dir
+            runtime_dir:    runtime_dir,
+            state_dir:      Some(state_dir)
         };
         Some(base_dirs)
     } else {
@@ -67,6 +69,7 @@ pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> {
         let data_local_dir = data_dir.clone();
         let preference_dir = config_dir.clone();
         let runtime_dir    = env::var_os("XDG_RUNTIME_DIR").and_then(dirs_sys::is_absolute_path).map(|o| o.join(&project_path));
+        let state_dir      = env::var_os("XDG_STATE_HOME") .and_then(dirs_sys::is_absolute_path).unwrap_or_else(|| home_dir.join(".local/state")).join(&project_path);
 
         let project_dirs = ProjectDirs {
             project_path:   project_path,
@@ -75,7 +78,8 @@ pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> {
             data_dir:       data_dir,
             data_local_dir: data_local_dir,
             preference_dir: preference_dir,
-            runtime_dir:    runtime_dir
+            runtime_dir:    runtime_dir,
+            state_dir:      Some(state_dir)
         };
         Some(project_dirs)
     } else {

src/mac.rs
@@ -75,6 +75,7 @@ pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> {
             data_local_dir: data_local_dir,
             preference_dir: preference_dir,
             runtime_dir:    None,
+            state_dir:      None
         };
         Some(project_dirs)
     } else {

commit 7290045c2cf5a2a0ad409ccd5ad302c17a5f4809

Fix breakage on Windows introduced by cb6a274.
`BaseDirs::state_dir` was added without updating the Windows code,
and tests were apparently never run.  `cargo check` now passes on my
Windows machine.

src/win.rs
@@ -24,7 +24,8 @@ pub fn base_dirs() -> Option<BaseDirs> {
             data_local_dir: data_local_dir,
             executable_dir: None,
             preference_dir: preference_dir,
-            runtime_dir:    None
+            runtime_dir:    None,
+            state_dir:      None
         };
         Some(base_dirs)
     } else {